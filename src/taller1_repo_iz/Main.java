6aebdacde3a6ff6c82b53f636b667252f9bbd130
Ismael <Ismael@host.docker.internal>
2022-05-18 11:31:44 -0500

    Archivos necesarios para el proyecto

---
+++ src/taller1_repo_iz/Main.java
@@ -0,0 +1,10 @@
+package taller1_repo_iz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer poyecto JAVA subido a GitHub");
+	}
+
+}
